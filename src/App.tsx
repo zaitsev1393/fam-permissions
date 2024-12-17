569c4443e9b9fed7411d89351a61ec9b2adcc94d
zaitsev1393 <zaitsev1393@gmail.com>
2024-12-17 16:50:27 +0200

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { permissions } from "./assets/permissions";
+import { Button } from "@/components/ui/button";
 
 export default function App() {
+  const sections = [{ name: "Full table" }, { name: "Tasks" }];
   return (
     <>
-      <h1 className="">
-        {permissions.map(({ name }) => (
-          <div>{name}</div>
-        ))}
-      </h1>
+      <div class="p-2">
+        <div className="flex gap-2">
+          {sections.map(({ name }) => (
+            <Button>{name}</Button>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
